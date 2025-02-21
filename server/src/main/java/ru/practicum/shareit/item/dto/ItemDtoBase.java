756deb5b07419608d6a6b292dba7331b96e69474
YulkaYa <yulkaya664@yandex.ru>
2025-02-21 13:03:32 +0300

    исправление на основе ревью: убрана валидация с dto ItemDtoBase в сервере

--- server/src/main/java/ru/practicum/shareit/item/dto/ItemDtoBase.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/ItemDtoBase.java
@@ -1,25 +1,16 @@
 package ru.practicum.shareit.item.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import ru.practicum.shareit.common.Create;
 import ru.practicum.shareit.common.StorageData;
 
 @SuperBuilder(toBuilder = true)
 @Data
 @RequiredArgsConstructor
 public class ItemDtoBase extends StorageData {
-    @NotBlank(groups = Create.class, message = "Название не может быть пустым")
     private String name;
-    @NotBlank(groups = Create.class, message = "Описание не может быть пустым")
-    @Pattern(regexp = ".*\\S+.*", message = "Описание не может состоять из пробелов или быть пустым")
     private String description;
-    @NotBlank(groups = Create.class, message = "Доступность не может быть пустой")
     private String available;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private long requestId;
 }
